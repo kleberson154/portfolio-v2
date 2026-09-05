5e2cb8d499efb4fd834088e5cc4d54d5f88f1be7
Kleberson Andrade <zed1544@hotmail.com>
2026-09-05 05:01:20 -0300

    feat: add route for SmartStock project page

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,10 +1,15 @@
 import { Routes, Route } from 'react-router-dom'
 import Home from '../pages/Home'
+import SmartStockProject from '../pages/SmartStockProject'
 
 export default function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route
+        path="/projects/smartstock"
+        element={<SmartStockProject />}
+      />
     </Routes>
   )
 }
