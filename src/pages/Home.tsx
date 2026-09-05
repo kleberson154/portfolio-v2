b928abf93d6c980a4ab37c48f18c4a17a07a3a88
Kleberson Andrade <zed1544@hotmail.com>
2026-09-05 04:21:59 -0300

    feat: create Home component with initial layout and styling

---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-[#050505] text-white">
+      <section className="min-h-screen flex items-center justify-center">
+        <h1 className="text-5xl font-bold text-green-400">
+          Kleberson Andrade
+        </h1>
+      </section>
+    </main>
+  )
+}
