e82c55bf1abbef4bb85ede0387d404c190223bfe
Kleberson Andrade <zed1544@hotmail.com>
2026-09-05 04:22:13 -0300

    feat: replace main layout with AppRoutes component for routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
+import AppRoutes from './routes/AppRoutes'
+
 function App() {
-  return (
-    <main className="min-h-screen bg-[#050505] text-white flex items-center justify-center">
-      <h1 className="text-5xl font-bold text-green-400">
-        Portfolio V2
-      </h1>
-    </main>
-  )
+  return <AppRoutes />
 }
 
 export default App
