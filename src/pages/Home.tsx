65f18aef3c7b16656adedc4fa1f62ac104887e2b
Kleberson Andrade <zed1544@hotmail.com>
2026-09-05 04:23:18 -0300

    feat: add Navbar component to Home page layout

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,20 @@
+import Navbar from '../components/layout/Navbar'
+
 export default function Home() {
   return (
-    <main className="min-h-screen bg-[#050505] text-white">
-      <section className="min-h-screen flex items-center justify-center">
-        <h1 className="text-5xl font-bold text-green-400">
-          Kleberson Andrade
-        </h1>
-      </section>
-    </main>
+    <>
+      <Navbar />
+
+      <main className="bg-[#050505] text-white">
+        <section
+          id="home"
+          className="min-h-screen flex items-center justify-center"
+        >
+          <h1 className="text-5xl font-bold text-green-400">
+            Kleberson Andrade
+          </h1>
+        </section>
+      </main>
+    </>
   )
 }
