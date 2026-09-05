5f2f0a9afb768e48f25c8e93a4b41ce691e7be16
Kleberson Andrade <zed1544@hotmail.com>
2026-09-05 05:01:15 -0300

    feat: add Projects section to Home page for project display

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,6 @@
 import Navbar from '../components/layout/Navbar'
 import Hero from '../sections/Hero'
+import Projects from '../sections/Projects'
 
 export default function Home() {
   return (
@@ -8,9 +9,9 @@ export default function Home() {
 
       <main className="bg-[#050505] text-white">
         <Hero />
+        <Projects />
 
         {/* Temporários para testar a Navbar */}
-        <section id="projects" className="min-h-screen" />
         <section id="technologies" className="min-h-screen" />
         <section id="about" className="min-h-screen" />
         <section id="experience" className="min-h-screen" />
